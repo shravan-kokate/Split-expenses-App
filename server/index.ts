996b01a5557e017037a3c67688443f02bb4bca07
Shravan Kokate <154724814+shravan-kokate@users.noreply.github.com>
2025-06-11 10:12:56 +0530

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -60,7 +60,7 @@ app.use((req, res, next) => {
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
   const port = 5000;
-  server.listen(port, "127.0.0.1", () => {
+  server.listen(port, "0.0.0.0", () => {
   log(`serving on port ${port}`);
 });
 })();
